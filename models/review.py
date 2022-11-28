81505f07caf46efb601dd07274fbbf0934fcb115
queens kisivuli <queenskisivuli@gmail.com>
2022-11-28 17:36:49 +0300

    revew model

---
+++ models/review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Review model definition
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Review object definitions
+
+    Attribute:
+        place_id: string - empty string: it will be the Place.id
+        user_id: string - empty string: it will be the User.id
+        text: string - empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
