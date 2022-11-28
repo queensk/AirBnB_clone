3e0be196184f6062fc5ac890dbe2efe79bf2c708
queens kisivuli <queenskisivuli@gmail.com>
2022-11-28 16:10:07 +0300

    updated users

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,13 @@
 Defines the FileStorage class.
 """
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+# from models.state import State
+# from models.city import City
+# from models.place import Place
+# from models.amenity import Amenity
+# from models.review import Review
 
 class FileStorage:
     """
@@ -20,7 +26,7 @@ class FileStorage:
         """
         Returns the dictionary object.
         """
-        return FileStorage._objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
