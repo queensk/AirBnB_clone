de66b72392960e556b6ec9d7570cf0a13873f6a3
queens kisivuli <queenskisivuli@gmail.com>
2022-11-28 16:19:33 +0300

    state class definiton

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+State module definition.
+"""
+from models.base_model import BaseModel
+
+
+class State:
+    """
+    Class define state
+
+    Attributes:
+        name: name of state (str).
+    """
+
+    name = ""
