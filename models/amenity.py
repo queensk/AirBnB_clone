8ad329ca6ffca3a4e78c37683787308ebf728690
queens kisivuli <queenskisivuli@gmail.com>
2022-11-28 16:43:02 +0300

    Amenity defintion

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Amenity class definition
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class definition
+    Attributes:
+        name: Amenity name (str)
+    """
+    name = ""
