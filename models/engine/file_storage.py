945fd23b3015d012ea3316598d5d116fca16ffbe
queens kisivuli <queenskisivuli@gmail.com>
2022-11-29 05:49:01 +0300

    fixed pycode style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage:
     """
     Represents an abstract file storage engin.
