17ed5adf0d065d5bda64b1212c24e6a11f937341
queens kisivuli <queenskisivuli@gmail.com>
2022-11-29 04:34:25 +0300

    updated console imports

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
 
 
 def parse(arg):
--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 """
 Defines the BaseModel class.
 """
